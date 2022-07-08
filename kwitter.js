fbefe76ed0aa386025c78965d53e0aaec69f4ed5
ThatRavenclawDemigod55 <87527408+ThatRavenclawDemigod55@users.noreply.github.com>
2022-07-08 11:57:36 +0100

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,7 @@
+function AddUser() {
+    user_name = document.getElementById("Username").value ; 
+
+    localStorage.setItem("user_name", user_name);
+
+    window.location = "kwitter_room.html" ;
+}
